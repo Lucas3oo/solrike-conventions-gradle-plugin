93ec7fbcece5da2190df7c1cf03e32d1ada71058
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-05-15 11:10:29 +0200

    add azure devops support

--- src/main/groovy/se/solrike/conventions/utils/CiUtils.groovy
+++ src/main/groovy/se/solrike/conventions/utils/CiUtils.groovy
@@ -8,14 +8,16 @@ import org.gradle.api.Project
 class CiUtils {
 
   /**
-   * @return build number or 'local' if not on Jenkins, Gitlab-CI, AWS CodeBuild
+   * @return build number or 'local' if not on Jenkins, Gitlab-CI, AWS CodeBuild, Azure DevOps
+   *
+   *
    */
   static String getBuildNumber()  {
-    return System.getenv('BUILD_NUMBER') ?: (System.getenv('CI_PIPELINE_ID') ?: (System.getenv('CODEBUILD_BUILD_NUMBER') ?: 'local'))
+    return System.getenv('BUILD_NUMBER') ?: (System.getenv('CI_PIPELINE_ID') ?: (System.getenv('CODEBUILD_BUILD_NUMBER') ?: (System.getenv('BUILD_BUILDNUMBER') ?: 'local')))
   }
 
   /**
-   * @return the branch name or empty string in case of main or a release branch
+   * @return the branch name or empty string in case of main or a release branch (which name ends with '_branch'
    */
   static String getBranchName(Project project) {
     String branchName = System.getenv('BRANCH_NAME') ?: project.grgit.branch.current().name
